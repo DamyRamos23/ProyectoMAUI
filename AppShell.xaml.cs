61e9a346e21ec2ef302a6d21de96a16efb8eb92b
DamyRamos23 <dany.ramos.ag@gmail.com>
2025-06-08 20:59:26 -0600

    Update AppShell.xaml.cs
    Se añade una ruta al detalle

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -7,6 +7,7 @@
             InitializeComponent();
             Routing.RegisterRoute(nameof(ListadoPacienteView), typeof(ListadoPacienteView));
             Routing.RegisterRoute(nameof(PacienteView), typeof(PacienteView));
+            Routing.RegisterRoute(nameof(CalculoPacienteView), typeof(CalculoPacienteView));
         }
     }
 }
